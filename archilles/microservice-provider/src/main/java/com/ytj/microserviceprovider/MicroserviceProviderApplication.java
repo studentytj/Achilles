fd66a9637c4f2fdf3ad73a8e3e83e5787c653ce1
studentytj <studentytj@163.com>
2019-03-14 22:55:29 +0800

    add eureka with three node, one provider, one gateway with zuul

---
+++ archilles/microservice-provider/src/main/java/com/ytj/microserviceprovider/MicroserviceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.ytj.microserviceprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceProviderApplication.class, args);
+    }
+
+}
